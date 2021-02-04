e6fe59a572e69b9e649b71eadd7312910d94424f
Takuma <iwgac1026@gmail.com>
2021-02-04 16:54:34 +0700

    change liff id

--- liff.js
+++ liff.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "i1655640405-7RPVLexV";
+    var liffId = "1655640405-7RPVLexV";
     initializeLiff(liffId);
 })
 
